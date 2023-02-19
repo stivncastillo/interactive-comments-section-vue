c98ed9b4082641b44a8b3c5e8131a05ebd5d9d14
Stiven <stivencastillo.90@gmail.com>
2023-02-18 21:16:33 -0500

    Init project

---
+++ src/App.vue
@@ -0,0 +1,213 @@
+<script setup lang="ts">
+import IconReply from "./components/icons/IconReply.vue";
+import IconDelete from "./components/icons/IconDelete.vue";
+import ButtonIcon from "./components/buttons/ButtonIcon.vue";
+</script>
+
+<template>
+  <main class="main-container">
+    <!-- Comments -->
+    <section class="comments-container">
+      <ul class="comments">
+        <li class="comment">
+          <div class="card">
+            <div class="counter">
+              <button class="counter-action">+</button>
+              <span>12</span>
+              <button class="counter-action">-</button>
+            </div>
+
+            <div class="comment-content">
+              <div class="comment-header">
+                <div class="comment-header-thumbnail">
+                  <img
+                    src="./assets/images/avatars/image-amyrobson.png"
+                    alt="Profile"
+                  />
+                </div>
+
+                <h2 class="h5">
+                  Name of user <span class="badge badge-primary">you</span>
+                </h2>
+                <span class="text-sm text-muted">1 month ago</span>
+
+                <div class="comment-actions">
+                  <ButtonIcon color="danger">
+                    <template #icon><IconDelete /></template>Delete</ButtonIcon
+                  >
+                  <ButtonIcon color="primary">
+                    <template #icon><IconReply /></template>Reply</ButtonIcon
+                  >
+                </div>
+              </div>
+
+              <p class="comment-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia aperiam vitae, cupiditate vero quae repellendus.
+                Consequatur itaque culpa quae? Minima id laboriosam sunt,
+                tempore distinctio laborum magni! Quaerat, recusandae earum.
+              </p>
+            </div>
+          </div>
+
+          <ul class="replies">
+            <li class="comment">
+              <div class="card">
+                <div class="counter">
+                  <button class="counter-action">+</button>
+                  <span>12</span>
+                  <button class="counter-action">-</button>
+                </div>
+
+                <div class="comment-content">
+                  <div class="comment-header">
+                    <div class="comment-header-thumbnail">
+                      <img
+                        src="./assets/images/avatars/image-amyrobson.png"
+                        alt="Profile"
+                      />
+                    </div>
+
+                    <h2 class="h5">Name of user</h2>
+                    <span class="text-sm text-muted">1 month ago</span>
+
+                    <div class="comment-actions">
+                      <ButtonIcon color="danger">
+                        <template #icon><IconDelete /></template
+                        >Delete</ButtonIcon
+                      >
+                      <ButtonIcon color="primary">
+                        <template #icon><IconReply /></template
+                        >Reply</ButtonIcon
+                      >
+                    </div>
+                  </div>
+
+                  <p class="comment-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia aperiam vitae, cupiditate vero quae repellendus.
+                    Consequatur itaque culpa quae? Minima id laboriosam sunt,
+                    tempore distinctio laborum magni! Quaerat, recusandae earum.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="comment">
+              <div class="card">
+                <div class="counter">
+                  <button class="counter-action">+</button>
+                  <span>12</span>
+                  <button class="counter-action">-</button>
+                </div>
+
+                <div class="comment-content">
+                  <div class="comment-header">
+                    <div class="comment-header-thumbnail">
+                      <img
+                        src="./assets/images/avatars/image-amyrobson.png"
+                        alt="Profile"
+                      />
+                    </div>
+
+                    <h2 class="h5">Name of user</h2>
+                    <span class="text-sm text-muted">1 month ago</span>
+
+                    <div class="comment-actions">
+                      <ButtonIcon color="danger">
+                        <template #icon><IconDelete /></template
+                        >Delete</ButtonIcon
+                      >
+                      <ButtonIcon color="primary">
+                        <template #icon><IconReply /></template
+                        >Reply</ButtonIcon
+                      >
+                    </div>
+                  </div>
+
+                  <p class="comment-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia aperiam vitae, cupiditate vero quae repellendus.
+                    Consequatur itaque culpa quae? Minima id laboriosam sunt,
+                    tempore distinctio laborum magni! Quaerat, recusandae earum.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="comment">
+              <div class="new-comment card">
+                <div class="avatar">
+                  <img
+                    src="./assets/images/avatars/image-amyrobson.png"
+                    alt="Avatar"
+                  />
+                </div>
+
+                <div class="comment-form">
+                  <textarea name="comment" id="comment" rows="3"></textarea>
+
+                  <ButtonIcon color="primary" fill size="lg">SEND</ButtonIcon>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+
+        <li class="comment">
+          <div class="card">
+            <div class="counter">
+              <button class="counter-action">+</button>
+              <span>12</span>
+              <button class="counter-action">-</button>
+            </div>
+
+            <div class="comment-content">
+              <div class="comment-header">
+                <div class="comment-header-thumbnail">
+                  <img
+                    src="./assets/images/avatars/image-amyrobson.png"
+                    alt="Profile"
+                  />
+                </div>
+
+                <h2 class="h5">Name of user</h2>
+                <span class="text-sm text-muted">1 month ago</span>
+
+                <div class="comment-actions">
+                  <ButtonIcon color="danger">
+                    <template #icon><IconDelete /></template>Delete</ButtonIcon
+                  >
+                  <ButtonIcon color="primary">
+                    <template #icon><IconReply /></template>Reply</ButtonIcon
+                  >
+                </div>
+              </div>
+
+              <p class="comment-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia aperiam vitae, cupiditate vero quae repellendus.
+                Consequatur itaque culpa quae? Minima id laboriosam sunt,
+                tempore distinctio laborum magni! Quaerat, recusandae earum.
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section class="new-comment-form">
+      <div class="new-comment card">
+        <div class="avatar">
+          <img src="./assets/images/avatars/image-amyrobson.png" alt="Avatar" />
+        </div>
+
+        <div class="comment-form">
+          <textarea name="comment" id="comment" rows="3"></textarea>
+
+          <ButtonIcon color="primary" fill size="lg">SEND</ButtonIcon>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/buttons/ButtonIcon.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { useSlots } from "vue";
+const slots = useSlots();
+</script>
+
+<template>
+  <button
+    class="container"
+    :class="{
+      primary: isPrimary,
+      secondary: isSecondary,
+      danger: isDanger,
+      [sizeClass]: size,
+      fill: fill,
+    }"
+  >
+    <i v-if="slots.icon">
+      <slot name="icon"> </slot>
+    </i>
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    color: { type: String, default: "primary" },
+    fill: { type: Boolean, default: false },
+    size: { type: String, default: "md" },
+  },
+  computed: {
+    isPrimary() {
+      return this.color === "primary";
+    },
+    isSecondary() {
+      return this.color === "secondary";
+    },
+    isDanger() {
+      return this.color === "danger";
+    },
+    sizeClass() {
+      return `size-${this.size}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  transition: 0.2s all ease;
+  background-color: transparent;
+
+  &:hover {
+    opacity: 0.7;
+  }
+}
+.container > span {
+  font-weight: 700;
+}
+
+.primary {
+  background-color: transparent;
+  color: var(--c-moderate-blue);
+}
+
+.secondary {
+  background-color: transparent;
+  color: var(--c-grayish-blue);
+}
+
+.danger {
+  background-color: transparent;
+  color: var(--c-soft-red);
+}
+
+.fill.primary {
+  background-color: var(--c-moderate-blue);
+  color: var(--c-white);
+}
+.fill.secondary {
+  background-color: var(--c-grayish-blue);
+  color: var(--c-white);
+}
+.fill.danger {
+  background-color: var(--c-soft-red);
+  color: var(--c-white);
+}
+
+.size-md {
+  padding: 6px 10px;
+  border-radius: 4px;
+}
+
+.size-lg {
+  padding: 10px 14px;
+  border-radius: 4px;
+  font-size: 12px;
+}
+
+.block {
+  display: block;
+  width: 100%;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  margin-right: 4px;
+}
+</style>
